c1f7582245af829b3967f9e9cbc47e70b199cec8
raisadwinda <raisa.dwinda@student.umn.ac.id>
2021-02-07 23:32:10 +0700

    fifth add

--- W02_33459/app/src/main/java/com/w02_33459/MainActivity.java
+++ W02_33459/app/src/main/java/com/w02_33459/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 //Penambahan class-class yang harus diimport
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -29,6 +31,15 @@ public class MainActivity extends AppCompatActivity {
         btnKali = (Button) this.findViewById(R.id.btnKali);
         btnBagi = (Button) this.findViewById(R.id.btnBagi);
         tvHasil = (TextView) this.findViewById(R.id.hasil);
+
+        etAngka1.addTextChangedListener(TambahTextWatcher);
+        etAngka1.addTextChangedListener(KurangTextWatcher);
+        etAngka1.addTextChangedListener(KaliTextWatcher);
+        etAngka1.addTextChangedListener(BagiTextWatcher);
+        etAngka2.addTextChangedListener(TambahTextWatcher);
+        etAngka2.addTextChangedListener(KurangTextWatcher);
+        etAngka2.addTextChangedListener(KaliTextWatcher);
+        etAngka2.addTextChangedListener(BagiTextWatcher);
 //        menambahkan interaksi terhadap button
         btnTambah.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -44,6 +55,76 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) { hitung( '/'); } } );
 
     }
+
+    private TextWatcher TambahTextWatcher = new TextWatcher() {
+      @Override
+      public void beforeTextChanged(CharSequence s, int start, int before, int count){
+
+      }
+      @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+          String angka1input = etAngka1.getText().toString();
+          String angka2input = etAngka2.getText().toString();
+
+          btnTambah.setEnabled(!angka1input.isEmpty() && !angka2input.isEmpty());
+      }
+      @Override
+        public void afterTextChanged(Editable s) {
+
+      }
+    };
+    private TextWatcher KurangTextWatcher = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int before, int count){
+
+        }
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+            String angka1input = etAngka1.getText().toString();
+            String angka2input = etAngka2.getText().toString();
+
+            btnKurang.setEnabled(!angka1input.isEmpty() && !angka2input.isEmpty());
+        }
+        @Override
+        public void afterTextChanged(Editable s) {
+
+        }
+    };
+    private TextWatcher KaliTextWatcher = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int before, int count){
+
+        }
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+            String angka1input = etAngka1.getText().toString();
+            String angka2input = etAngka2.getText().toString();
+
+            btnKali.setEnabled(!angka1input.isEmpty() && !angka2input.isEmpty());
+        }
+        @Override
+        public void afterTextChanged(Editable s) {
+
+        }
+    };
+    private TextWatcher BagiTextWatcher = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int before, int count){
+
+        }
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+            String angka1input = etAngka1.getText().toString();
+            String angka2input = etAngka2.getText().toString();
+
+            btnBagi.setEnabled(!angka1input.isEmpty() && !angka2input.isEmpty());
+        }
+        @Override
+        public void afterTextChanged(Editable s) {
+
+        }
+    };
+
     protected void hitung(char operator) {
         double dbAngka1 = Double.parseDouble(etAngka1.getText().toString());
         double dbAngka2 = Double.parseDouble(etAngka2.getText().toString());
