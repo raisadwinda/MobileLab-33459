884e98a12caf9173a79918c5b37cb49ea90af710
raisadwinda <raisa.dwinda@student.umn.ac.id>
2021-02-19 13:50:56 +0700

    first add

---
+++ W04_33459/app/src/main/java/com/w04_33459/SecondActivity.java
@@ -0,0 +1,27 @@
+package com.w04_33459;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class SecondActivity extends AppCompatActivity {
+
+//    TextView tvTesAja;
+//    String strTes;
+//
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+//        //nerima intent yg dikirim main activity
+//        Intent intent = getIntent();
+//        strTes = intent.getStringExtra("tesss");
+//
+//        tvTesAja = findViewById(R.id.TesAja);
+//
+//        tvTesAja.setText(strTes);
+    }
+}
